98c9347fc4f8d1adf0533042b98c5eed1ef23827
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-06-06 21:56:58 +0200

    Nieuwe style bootstrap

--- resources/views/admin/approvals/index.blade.php
+++ resources/views/admin/approvals/index.blade.php
@@ -1,43 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Aanvragen</h1>
+    <div class="container">
+        <h1 class="my-3">Aanvragen</h1>
 
-    @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
-        </div>
-    @endif
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
 
-    <table>
-        <thead>
-            <tr>
-                <th>Naam</th>
-                <th>Email</th>
-                <th>Acties</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $user)
+        <table class="table table-striped">
+            <thead>
                 <tr>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->role }}</td>
-                    <td>
-                        <form action="{{ route('admin.approvals.approve', $user) }}" method="post">
-                            @csrf
-                            <button type="submit">Goedkeuren</button>
-                        </form>
-                        <form action="{{ route('admin.approvals.delete', $user) }}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">Verwijderen</button>
-                        </form>
-                        
-                    </td>
+                    <th>Naam</th>
+                    <th>Email</th>
+                    <th>Rol</th>
+                    <th>Acties</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @forelse ($users as $user)
+                    <tr>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->role }}</td>
+                        <td>
+                            <div class="d-flex">
+                                <form action="{{ route('admin.approvals.approve', $user) }}" method="post" class="mr-2">
+                                    @csrf
+                                    <button type="submit" class="btn btn-sm btn-success">Goedkeuren</button>
+                                </form>
+                                <form action="{{ route('admin.approvals.delete', $user) }}" method="post" onsubmit="return confirm('Weet je zeker dat je deze gebruiker wilt verwijderen?');">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-danger">Verwijderen</button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="4">Er zijn momenteel geen aanvragen.</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 @endsection
 
