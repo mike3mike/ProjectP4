93858b525fe1915d9b71c6374e451f3dea9753f4
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-06-07 22:56:11 +0200

    Wijzigingen in RoleMiddleware voor validatieregels

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -14,9 +14,11 @@ class RoleMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, $role): Response
+    //...$roles
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        if (! Auth::check() || $request->user()->role != $role) {
+        // if (! Auth::check() || $request->user()->role != $role) 
+        if (! Auth::check() || ! in_array($request->user()->role, $roles)){
             // De gebruiker is niet ingelogd of heeft niet de juiste rol.
             // Je kunt hier ook een redirect naar een andere pagina uitvoeren als je dat wilt.
             abort(403, 'U heeft geen toegang tot deze pagina.');
