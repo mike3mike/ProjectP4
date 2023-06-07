ee00fc5249ab7b17b0c1ceb10b735342697b0e0f
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-06-07 22:56:11 +0200

    Wijzigingen in migratiebestand voor validatieregels

--- database/migrations/2023_06_02_000003_create_task_table.php
+++ database/migrations/2023_06_02_000003_create_task_table.php
@@ -9,16 +9,18 @@ class CreateTaskTable extends Migration
     {
         Schema::create('task', function (Blueprint $table) {
             $table->id();
+            $table->string('task_name');
             $table->time('begin_time');
             $table->time('end_time');
             $table->date('date');
             $table->string('description');
             $table->string('instructor_name');
             $table->integer('task_number');
+            $table->string('play_address_name');
             $table->foreignId('play_address_id')->constrained('address');
             $table->foreignId('makeup_address_id')->nullable()->constrained('address');
             $table->enum('status', ['lopend','inBehandeling','afgerond'])->default('lopend');
-            $table->enum('task_type', ['BHV', 'EHBO', 'Examen']);
+            $table->json('task_type');
             $table->foreignId('client_id')->constrained('client', 'user_id');
             $table->timestamps();
         });
