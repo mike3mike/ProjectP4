47b537580d066b8a3845b8e7c115832bcd461099
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-06-09 00:53:42 +0200

    Update: Wijzigingen in UserTask model toegepast

--- app/Models/UserTask.php
+++ app/Models/UserTask.php
@@ -9,11 +9,14 @@ class UserTask extends Model
 {
     use HasFactory;
     protected $table = 'user_task';
-    protected $fillable = ['user_id', 'status', 'admit', 'task_id'];
+    protected $fillable = ['user_id', 'status', 'admit', 'task_id','assigned_by'];
     public function user() {
         return $this->belongsTo(User::class, 'user_id');
     }
     public function task() {
         return $this->belongsTo(Task::class, 'task_id');
+    }
+    public function coordinator() {
+        return $this->belongsTo(User::class, 'assigned_by');
     }
 }
