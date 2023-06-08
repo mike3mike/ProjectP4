4782f552f3b804d8b92855761b13a611b4f921b7
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-06-09 00:53:43 +0200

    Update: Weergave voor openstaande opdrachten aangepast voor leden

--- resources/views/lid/openAssignments.blade.php
+++ resources/views/lid/openAssignments.blade.php
@@ -1,48 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Uitnodigingen</h1>
+    <div class="container">
+        <h1 class="my-3">Uitnodigingen</h1>
 
-    @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
-        </div>
-    @endif
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
 
-    <table>
-        <thead>
-            <tr>
-                <th>info1</th>
-                <th>info2</th>
-                <th>info3</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($userTasks as $userTask)
+        <table class="table table-bordered table-striped">
+            <thead>
                 <tr>
-                    <td>{{ $userTask->id }}</td>
-                    <td>{{ $userTask->status }}</td>
-                    <td>{{ $userTask->admit }}</td>
-                    <td>
-                        <form action="{{ route('member.openAssignments.accept', $userTask) }}" method="post">
-                            @csrf
-                            <button type="submit">Accepteren</button>
-                        </form>
-                    </td>
-                    <td>
-                        <form action="{{ route('member.openAssignments.maybe', $userTask) }}" method="post">
-                            @csrf
-                            <button type="submit">Ik weet het nog niet</button>
-                        </form>
-                    </td>
-                    <td>
-                        <form action="{{ route('member.openAssignments.decline', $userTask) }}" method="post">
-                            @csrf
-                            <button type="submit">Weigeren</button>
-                        </form>
-                    </td>
+                    <th>info1</th>
+                    <th>info2</th>
+                    <th>info3</th>
+                    <th>Acties</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($userTasks as $userTask)
+                    <tr>
+                        <td>{{ $userTask->id }}</td>
+                        <td>{{ $userTask->status }}</td>
+                        <td>{{ $userTask->admit }}</td>
+                        <td>
+                            <form action="{{ route('member.openAssignments.accept', $userTask) }}" method="post">
+                                @csrf
+                                <button type="submit" class="btn btn-success">Accepteren</button>
+                            </form>
+                            <form action="{{ route('member.openAssignments.maybe', $userTask) }}" method="post" class="mt-2">
+                                @csrf
+                                <button type="submit" class="btn btn-warning">Ik weet het nog niet</button>
+                            </form>
+                            <form action="{{ route('member.openAssignments.decline', $userTask) }}" method="post" class="mt-2">
+                                @csrf
+                                <button type="submit" class="btn btn-danger">Weigeren</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
