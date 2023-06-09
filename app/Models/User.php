3d675059aca4aec7cca4907d638cdfa586864267
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-06-09 12:40:27 +0200

    Update User model to include new relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,6 +58,11 @@ class User extends Authenticatable
     {
         return $this->roles()->where('name', $role)->exists();
     }
+    public function userTasks()
+{
+    return $this->hasMany(UserTask::class);
+}
+
 }
 
 
