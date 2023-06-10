e0aaf41a93449361f093b0f570489400145e2481
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-06-10 17:24:20 +0200

    Bijgewerkt web routes met nieuwe routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminApprovalController;
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Admin\AdminApprovalController;
+use App\Http\Controllers\Admin\RoleRequestController;
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
@@ -36,38 +37,31 @@ Route::middleware(['auth', 'role:coordinator'])->group(function () {
     Route::get('/admin/new-assignments', [AdminApprovalController::class, 'getAssignmentRequests'])->name('admin.approvals.getAssignmentsRequests');
     Route::post('/admin/new-assignments/{task}', [AdminApprovalController::class, 'approveAssignment'])->name('admin.approvals.approveAssignment');
     Route::get('/admin/new-assignments/{task}', [AdminApprovalController::class, 'inviteMember'])->name('admin.approvals.inviteMember');
-    Route::get('/admin/task/{id}', [TaskController::class, 'showAdmin'])->name('task.show_task_details_admin');
-    Route::get('admin/{task}/invite', [TaskController::class, 'invite'])->name('admin.invite');
-    Route::post('admin/{task}/invite', [TaskController::class, 'sendInvitation'])->name('admin.sendInvitation');
+    Route::get('/admin/task/{id}', [AdminTaskController::class, 'showAdmin'])->name('task.show_task_details_admin');
+    Route::get('admin/{task}/invite', [AdminTaskController::class, 'invite'])->name('admin.invite');
+    Route::post('admin/{task}/invite', [AdminTaskController::class, 'sendInvitation'])->name('admin.sendInvitation');
+    Route::get('/admin/role-requests', [RoleRequestController::class, 'index'])->name('admin.role-requests.index');
+    Route::post('/admin/role-requests/{user}/{role}/approve',  [RoleRequestController::class, 'approve'])->name('admin.role-requests.approve');
+    Route::post('/admin/role-requests/{user}/{role}/deny', [RoleRequestController::class, 'deny'])->name('admin.role-requests.deny');
+    
 });
-
-
-// Route::middleware(['auth', 'role:coordinator'])->group(function () {
-//     Route::get('/admin/approvals', [AdminApprovalController::class, 'index'])->name('admin.approvals.index');
-//     Route::post('/admin/approvals/{user}', [AdminApprovalController::class, 'approve'])->name('admin.approvals.approve');
-//     Route::delete('/admin/approvals/{user}', [AdminApprovalController::class, 'destroy'])->name('admin.approvals.delete');
-//     Route::get('/admin/new-assignments', [AdminApprovalController::class, 'getAssignmentRequests'])->name('admin.approvals.getAssignmentsRequests');
-//     Route::post('/admin/new-assignments/{task}', [AdminApprovalController::class, 'approveAssignment'])->name('admin.approvals.approveAssignment');
-//     Route::get('/admin/new-assignments/{task}', [AdminApprovalController::class, 'inviteMember'])->name('admin.approvals.inviteMember');
-//     Route::get('/admin/task/{id}', [TaskController::class, 'showAdmin'])->name('task.show_task_details_admin');
-//     Route::get('admin/{task}/invite', [TaskController::class, 'invite'])->name('admin.invite');
-//     Route::post('admin/{task}/invite', [TaskController::class, 'sendInvitation'])->name('admin.sendInvitation');
-// });
 
 Route::middleware(['auth', 'role:lid'])->group(function () {
     Route::get('/member/open-assignments', [UserController::class, 'index'])->name('member.openAssignments.index');
     Route::post('/member/open-assignments/accept/{userTask}', [UserController::class, 'accept'])->name('member.openAssignments.accept');
     Route::post('/member/open-assignments/maybe/{userTask}', [UserController::class, 'maybe'])->name('member.openAssignments.maybe');
     Route::post('/member/open-assignments/decline/{userTask}', [UserController::class, 'decline'])->name('member.openAssignments.decline');
+    Route::post('/member/become_client', [UserController::class, 'submitBecomeClient'])->name('task.submit_become_client');
+    Route::get('/member/become_client', [UserController::class, 'memberBecomeClient'])->name('member.become_client');
+    Route::get('/member/check_client_status',[UserController::class, 'checkClientStatus'])->name('member.check_client_status');
 });
 Route::middleware(['auth', 'role:opdrachtgever'])->group(function () {
-    Route::post('task', [TaskController::class, 'store'])->name('task.store');
-    Route::get('task', [TaskController::class, 'index'])->name('task.index');
-    Route::get('task/create', [TaskController::class, 'create'])->name('task.create');
-    Route::get('/task/{id}', [TaskController::class, 'show'])->name('task.show');
+    Route::post('client/task', [ClientController::class, 'store'])->name('task.store');
+    Route::get('client/task', [ClientController::class, 'index'])->name('task.index');
+    Route::get('client/task/create', [ClientController::class, 'create'])->name('task.create');
+    Route::get('client//task/{id}', [ClientController::class, 'show'])->name('task.show');
 });
 
 
-Route::post('/task/become_client', [TaskController::class, 'submitBecomeClient'])->name('task.submit_become_client');
 
 
