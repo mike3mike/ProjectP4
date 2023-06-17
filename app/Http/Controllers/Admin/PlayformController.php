54f7a64312c93a93458595d9a9facf76349e2ebb
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-06-17 21:17:33 +0200

    Update PlayformController

--- app/Http/Controllers/Admin/PlayformController.php
+++ app/Http/Controllers/Admin/PlayformController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Task;
 use PDF; // Use the PDF facade we have set up
 
 class PlayformController extends Controller
