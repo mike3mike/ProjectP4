4891363aa0775f5b52958db7ff2a1a7a3cf717e5
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-06-17 16:23:43 +0200

    Update ex view with improved layout

--- resources/views/admin/tasks/ex.blade.php
+++ resources/views/admin/tasks/ex.blade.php
@@ -36,9 +36,9 @@
                                 @csrf
                                 <button type="submit" class="btn btn-success">Goedkeuren</button>
                             </form>
-                            <form action="{{ route('admin.tasks.remove', $userTask) }}" method="post" class="mt-2">
+                            <form action="{{ route('admin.tasks.remove', $userTask) }}" method="post" class="mt-2" >
                                 @csrf
-                                <button type="submit" class="btn btn-secondary">Verwijderen</button>
+                                <button type="submit" class="btn btn-secondary" onclick="confirmDeletion(event, this.parentElement);">Verwijderen</button>
                             </form>
                             <form action="{{ route('admin.tasks.details', $userTask) }}" method="get" class="mt-2">
                                 <button type="submit" class="btn btn-info">Details</button>
@@ -49,4 +49,24 @@
         </tbody>
     </table>
 </div>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script>
+    function confirmDeletion(event, form) {
+        event.preventDefault();
+        Swal.fire({
+            title: 'Weet je het zeker?',
+            text: "Je zult deze actie niet kunnen terugdraaien!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ja, verwijder het!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                form.submit();
+            }
+        })
+    }
+    </script>    
 @endsection
+{{-- onsubmit="return confirm('Weet je zeker dat je deze gebruikerstaak wilt verwijderen?');" --}}
