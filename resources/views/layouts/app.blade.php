a370c10b099345b1a17bd1086abf3e8ece2bf021
samadov2201 <muhammadsamadov@gmail.com>
2023-06-17 18:35:02 +0200

    fixed logout bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -94,6 +94,7 @@
               </div>
                                                   <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                                     @csrf
+                                                  </form>
           </li>
 
       </ul>
