5aeb086b0c18e94c3dff04bfc63e1d172b5eb90c
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-06-18 18:23:21 +0200

    Translate names to Dutch in become_client view

--- resources/views/member/become_client.blade.php
+++ resources/views/member/become_client.blade.php
@@ -10,14 +10,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Become a Client') }}</div>
+                <div class="card-header">{{ __('client worden') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('task.submit_become_client') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="company_name" class="col-md-4 col-form-label text-md-right">{{ __('Company Name') }}</label>
+                            <label for="company_name" class="col-md-4 col-form-label text-md-right">{{ __('Bedrijfsnaam') }}</label>
 
                             <div class="col-md-6">
                                 <input id="company_name" type="text" placeholder="Voer uw bedrijf naam in" class="form-control @error('company_name') is-invalid @enderror" name="company_name" value="{{ old('company_name') }}" required autocomplete="company_name" autofocus>
@@ -31,7 +31,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="contact_person" class="col-md-4 col-form-label text-md-right">{{ __('Contact Person') }}</label>
+                            <label for="contact_person" class="col-md-4 col-form-label text-md-right">{{ __('Contactpersoon') }}</label>
 
                             <div class="col-md-6">
                                 <input id="contact_person" placeholder="Voer de naam van de contactpersoon in" type="text" class="form-control @error('contact_person') is-invalid @enderror" name="contact_person" value="{{ old('contact_person') }}">
@@ -45,7 +45,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="contact_person_phone" class="col-md-4 col-form-label text-md-right">{{ __('Contact Person Phone') }}</label>
+                            <label for="contact_person_phone" class="col-md-4 col-form-label text-md-right">{{ __('Contactpersoon Telefoon') }}</label>
 
                             <div class="col-md-6">
                                 <input id="contact_person_phone" type="text" placeholder="Voer het telefoonnummer van de contactpersoon in" class="form-control @error('contact_person_phone') is-invalid @enderror" name="contact_person_phone" value="{{ old('contact_person_phone') }}">
@@ -58,7 +58,7 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="billing_email" class="col-md-4 col-form-label text-md-right">{{ __('Billing Email') }}</label>
+                            <label for="billing_email" class="col-md-4 col-form-label text-md-right">{{ __('Factuur e-mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="billing_email" type="text" placeholder="Voer uw facturatie e-mailadres in" class="form-control @error('billing_email') is-invalid @enderror" name="billing_email" value="{{ old('billing_email') }}">
@@ -71,7 +71,7 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="city" class="col-md-4 col-form-label text-md-right">{{ __('City') }}</label>
+                            <label for="city" class="col-md-4 col-form-label text-md-right">{{ __('Stad') }}</label>
 
                             <div class="col-md-6">
                                 <input id="city" type="text" placeholder="Voer uw stad in" class="form-control @error('city') is-invalid @enderror" name="city" value="{{ old('city') }}">
@@ -84,7 +84,7 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="street" class="col-md-4 col-form-label text-md-right">{{ __('Street') }}</label>
+                            <label for="street" class="col-md-4 col-form-label text-md-right">{{ __('Straat') }}</label>
 
                             <div class="col-md-6">
                                 <input id="street" type="text" placeholder="Voer uw straatnaam in" class="form-control @error('street') is-invalid @enderror" name="street" value="{{ old('street') }}">
@@ -98,7 +98,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="postal_code" class="col-md-4 col-form-label text-md-right">{{ __('Postal Code') }}</label>
+                            <label for="postal_code" class="col-md-4 col-form-label text-md-right">{{ __('Postcode') }}</label>
 
                             <div class="col-md-6">
                                 <input id="postal_code" type="text" placeholder="Voer uw postcode in" class="form-control @error('postal_code') is-invalid @enderror" name="postal_code" value="{{ old('postal_code') }}">
@@ -112,7 +112,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="house_number" class="col-md-4 col-form-label text-md-right">{{ __('House Number') }}</label>
+                            <label for="house_number" class="col-md-4 col-form-label text-md-right">{{ __('Huisnummer') }}</label>
 
                             <div class="col-md-6">
                                 <input id="house_number" type="text" placeholder="Voer uw huisnummer in" class="form-control @error('house_number') is-invalid @enderror" name="house_number" value="{{ old('house_number') }}">
@@ -128,7 +128,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Submit') }}
+                                    {{ __('Verzenden') }}
                                 </button>
                             </div>
                         </div>
