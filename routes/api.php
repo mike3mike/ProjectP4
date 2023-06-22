151141bb6100c2852f5cbc18e4ee2ac7cfd6e111
Steen010 <kjh.vansteen@student.avans.nl>
2023-06-22 02:50:26 +0200

    update

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | API Routes
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
